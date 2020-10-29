b8def90acc738b7c1f7882ceb115e479a7d4e999
cemalcakir <69013894+cemalcakir@users.noreply.github.com>
2020-10-29 03:11:21 +0300

    Update fizz.py

--- fizz.py
+++ fizz.py
@@ -1,6 +1,7 @@
 def fizzBuzz(**kwargs):
 	for i in range(1, 101):
 		output = ""
+		# Keys can not be integers in Python dictionaries, so I used value as key.
 		for key, value in kwargs.items():
 			if not i % value:
 				output += key
