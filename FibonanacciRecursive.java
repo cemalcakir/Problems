9464f9f480ab9b60f33a1d837c7c93a1833d235e
cemalcakir <69013894+cemalcakir@users.noreply.github.com>
2021-08-02 14:34:15 +0300

    Update FibonanacciRecursive.java

--- FibonanacciRecursive.java
+++ FibonanacciRecursive.java
@@ -1,12 +1,24 @@
 public class FibonanacciRecursive {
 	public static void main(String[] args) {
-		fibonacciRecursive(5);
+		System.out.println(fibonacci(5));
 	}
-
-	public static int fibonacciRecursive(int n) {
+	
+	// Main function
+	public static int fibonacci(int n) {
+		return fibonacciRecursive(n, new int[n + 1]);
+	}
+	
+	// Helper function
+	public static int fibonacciRecursive(int n, int[] array) {
 		if (n == 0 || n == 1) {
 			return 1;
 		}
-		return fibonacciRecursive(n - 2) + fibonacciRecursive(n - 1);
+		
+		// Memoization
+		if (array[n] == 0) {
+			int result = fibonacciRecursive(n - 2, array) + fibonacciRecursive(n - 1, array);
+			array[n] = result;
+		}
+		return array[n];
 	}
 }
